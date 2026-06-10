7cd48d4782d2ca139a28d1505b915e8c3035f36f
yas0905 <yasmim.faville@escola.pr.gov.br>
2026-06-10 13:38:49 -0300

    alteração 2

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-function mostrarCuriosidade(){document.getElementById("texto").innerText="Sistemas automatizados podem reduzir significativamente o desperdício de água na agricultura.";}
+function mostrarCuriosidade(){document.getElementById("texto").innerText="Sistemas de irrigação automatizados podem reduzir significativamente o desperdício de água na agricultura, fornecendo a quantidade ideal de água no momento certo. Essa tecnologia aumenta a eficiência no uso dos recursos hídricos, contribui para a sustentabilidade e auxilia na produção de alimentos de forma mais responsável.";}
